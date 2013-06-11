2c9c9b432ba55a9fc2f60c31e3b542d9d4f09610
Daria Kuntsevich <daria.m.kuntsevich@gmail.com>
2013-06-11 18:43:24 +0400

    Added correct email address to the send form

--- feedbackForm.php
+++ feedbackForm.php
@@ -1,19 +1,20 @@
 <?php
 	$owner_email = 'podguznik2014@yandex.ru';
-	$headers = 'From:' . $_POST["email"];
-	$subject = 'A message from your site visitor ' . $_POST["name"];
+	$headers = 'From: postmaster@dzd-nn.ru
+Reply-To: postmaster@dzd-nn.ru
+Content-Type: text/plain; charset=windows-1251
+Content-Transfer-Encoding: 8bit';
+	$subject = 'A message from your site visitor ' . $_POST["name"] . ' <' . $_POST["email"] . '>';
 	$messageBody = "";
 	
-	$messageBody .= '<p>Visitor: ' . $_POST["name"] . '</p>' . "\n";
-	$messageBody .= '<br>' . "\n";
-	$messageBody .= '<p>Email Address: ' . $_POST['email'] . '</p>' . "\n";
-	$messageBody .= '<br>' . "\n";
-	$messageBody .= '<p>Message: ' . $_POST['message'] . '</p>' . "\n";
-	
-	if($_POST["stripHTML"] == 'true'){
-		$messageBody = strip_tags($messageBody);
-	}
-	
+	$messageBody .= 'Visitor: ' . $_POST["name"] . '' . "\n";
+	$messageBody .= '' . "\n";
+	$messageBody .= 'Email Address: ' . $_POST['email'] . '' . "\n";
+	$messageBody .= '' . "\n";
+	$messageBody .= 'Message: ' . $_POST['message'] . '' . "\n";
+
+	$messageBody = strip_tags($messageBody);
+
 	try{
 		if(!mail($owner_email, $subject, $messageBody, $headers)){
 			throw new Exception('mail failed');
